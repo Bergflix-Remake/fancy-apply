aa7dfd23c6c1da1e22f1451b7d10fb0c7685ad0f
AnnikenYT <anniken@mooonshine.net>
2022-08-04 13:36:36 +0000

    feat: :sparkles: syntax highlighting

--- src/components/Codesnippet.vue
+++ src/components/Codesnippet.vue
@@ -3,11 +3,11 @@
     class="flex flex-col lg:w-3/4 w-full p-5 bg-[#282c34] shadow-lg rounded-md space-y-4"
   >
     <div class="flex flex-row justify-start p-1 space-x-2">
-      <div class="mr-auto flex flex-row space-x-2 justify-center">
+      <div class="flex flex-row justify-center mr-auto space-x-2">
         <svg
           @click="handleSubmit"
           xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 text-neutral-400 hover:text-green-300 cursor-pointer"
+          class="w-6 h-6 cursor-pointer text-neutral-400 hover:text-green-300"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -22,17 +22,17 @@
         </svg>
         <h1 class="text-sm text-neutral-400 text-italic">{{ filename }}</h1>
       </div>
-      <div class="ml-auto flex flex-row space-x-2">
-        <div class="w-4 shadow-sm h-4 aspect-square rounded-full bg-yellow-500"></div>
-        <div class="w-4 shadow-sm h-4 aspect-square rounded-full bg-green-500"></div>
-        <div class="w-4 shadow-sm h-4 aspect-square rounded-full bg-red-500"></div>
+      <div class="flex flex-row ml-auto space-x-2">
+        <div class="w-4 h-4 bg-yellow-500 rounded-full shadow-sm aspect-square"></div>
+        <div class="w-4 h-4 bg-green-500 rounded-full shadow-sm aspect-square"></div>
+        <div class="w-4 h-4 bg-red-500 rounded-full shadow-sm aspect-square"></div>
       </div>
     </div>
     <Editor
       height="25vh"
       theme="vs-dark"
       :value="text"
-      language="html"
+      :language="language"
       class-name="rounded-md"
       @mount="(editor) => (monacoRef = editor)"
       @update:value="(value) => (text = value as string)"
@@ -54,6 +54,7 @@
 
 <script lang="ts" setup>
 import Editor from "@guolao/vue-monaco-editor";
+import { computed } from "@vue/reactivity";
 import { defineProps, Ref, ref, watch } from "vue";
 const emit = defineEmits(["submit"]);
 const props = defineProps({
@@ -79,6 +80,21 @@ const handleSubmit = () => {
   console.log("Submitting with this data: \n", text.value);
   emit("submit", [text.value]);
 };
+const language = computed(() => {
+  const fileExtension = props.filename.split(".")[1];
+  switch (fileExtension) {
+    case "ts":
+      return "typescript";
+    case "vue":
+      return "html";
+    case "bash":
+      return "shell";
+    case "json":
+      return "json";
+    default:
+      return "html";
+  };
+})
 </script>
 
 <style>
